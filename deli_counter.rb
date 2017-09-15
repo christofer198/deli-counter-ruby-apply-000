1094e6bcd93927833cff14089375df7ea14f1a14
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-15 12:16:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,25 +1,18 @@
-
-def line(array)
-  if array == []
-    puts "The line is currently empty."
-  else
-    stringy = ""
-    array.each_with_index{|name, index| stringy += " #{index+1}. #{name}"}
-    puts "The line is currently:#{stringy}"
- end
-end
-
+katz_deli = []
 def take_a_number(array, name)
 
   array.push(name)
-  puts "Welcome, #{name}. You are number #{ array.length } in line."
+  puts = "Welcome #{name}. You are number #{array.length} in line"
+end
 
+def line(array)
+
+  output = ""
+  array.each_with_index{|val, ind| output += " #{val}. #{index}"}
+  puts "The line is currently:#{output}"
 end
 
 def now_serving(array)
-  if array == []
-    puts "There is nobody waiting to be served!"
-  else
-	   puts "Currently serving #{array.shift}."
-  end
+
+  puts "Currently Serving #{array.shift}"
 end
