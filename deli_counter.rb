abb24992249df0f9fbfec8176027416d4876836d
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-15 12:21:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,10 @@
 katz_deli = []
+
 def take_a_number(array, name)
 
   array.push(name)
-  puts = "Welcome #{name}. You are number #{array.length} in line"
+  puts "Welcome, #{name}. You are number #{ array.length } in line."
+
 end
 
 def line(array)
